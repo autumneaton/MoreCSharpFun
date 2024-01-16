592a064efe8c1a15813ea7bc2204784812e69251
autumneaton <autumneaton@AUTUMNEATON3476>
2024-01-16 16:55:34 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+        private string language;
+        // constructor
+        
+        public PrintStuff()
+        {
+            language = "EN";
+        }
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+        public void PrintName()
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello John or Jane Doe!");
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi John or Jane Doe!");
+            }
+
+        }
+
+        public void PrintName(string n)
+        {
+            if (language == "EN")
+            {
+                System.Console.WriteLine("Hello, " + n + "!");
+            }
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + n + "!");
+            }
+
+        }
+    }
+}
